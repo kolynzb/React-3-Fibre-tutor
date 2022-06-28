4ab1e9d8d48191776fcde8b50f69557b075fd189
kolynzb <collinsbenda360@gmail.com>
2022-06-28 13:39:13 +0300

    Creat e custom cursor

---
+++ components/Cursor.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+const cursor = (props: Props) => {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+  useEffect(() => {
+    const mouseMove = (e: any) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  const variants: any = {
+    default: {
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+    },
+    text: {
+      height: 150,
+      width: 150,
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      backgroundColor: "yellow",
+      mixBlendMode: "difference",
+    },
+  };
+
+  const textEnter = () => setCursorVariant("text");
+  const textLeave = () => setCursorVariant("default");
+
+  return (
+    <>
+      <h1 onMouseEnter={textEnter} onMouseLeave={textLeave} className="title">
+        Hello World
+      </h1>
+      <motion.div
+        className="bg-black h-8 w-8 rounded-full fixed top-0 left-0  "
+        variants={variants}
+        animate={cursorVariant}
+      />
+    </>
+  );
+};
+
+export default cursor;
---
+++ tut.md
@@ -0,0 +1,6 @@
+https://tailwind-elements.com/quick-start/
+
+- Custom Cursor
+  https://youtu.be/nr3U-RpaQuM?list=TLPQMjgwNjIwMjK7vyRYtHDtsA
+  https://github.com/nicubarbaros/custom-mouse-change/tree/master/src/components
+  https://javascript.plainenglish.io/how-to-create-a-custom-cursor-in-react-272314682285
